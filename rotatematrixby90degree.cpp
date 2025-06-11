1d4a476f838bde773ba70c897822992563a33a0e
shivangini09 <b22ph012@iitj.ac.in>
2025-06-11 22:34:29 +0530

    updated matrix by 90 deg soln

--- rotatematrixby90degree.cpp
+++ rotatematrixby90degree.cpp
@@ -5,7 +5,7 @@ public:
         // transpose of matrix
         for(int i=0; i<n-1; i++){
             for(int j=i+1; j<n; j++){
-                swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j],matrix[j][i]);       // in transpose, diagonal elements remain same 
             }
         }
 
