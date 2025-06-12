25ccde327bbaf7bab489e06a5be49004ed55c12a
shivangini09 <b22ph012@iitj.ac.in>
2025-06-12 15:11:31 +0530

    added buy and sell stock 1 soln

--- buyandsellstock1.cpp
+++ buyandsellstock1.cpp
@@ -5,9 +5,9 @@ public:
         int mini = prices[0];
         int n = prices.size();
         for(int i=1; i<n; i++){
-            int cost = prices[i]-mini;
+            int cost = prices[i]-mini;          // maxprofit that can be extracted from selling stock on that day!
             maxprofit = max(maxprofit,cost);
-            mini = min(mini,prices[i]);
+            mini = min(mini,prices[i]);                     // update the minimum
         }
         return maxprofit;
         
