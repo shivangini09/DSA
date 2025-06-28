9ee374a062a6c360ce9a2338f06281ebe76a0a39
shivangini09 <b22ph012@iitj.ac.in>
2025-06-29 03:27:03 +0530

    added course schedule 2 soln

---
+++ courseschedule2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        int V = numCourses;
+        for(auto it: prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].push_back(u);  // reverse edges given
+        }
+        vector<int> indegree(V,0);
+        for(int i=0; i<V; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;   // jitni baar we find an element in adj that means utni nodes ussme aa rhi h
+            }
+        }
+        
+        queue<int> q;
+        for(int i=0; i<V; i++){
+            if(indegree[i]==0){
+                q.push(i);    // push the orphanns 
+            }
+        }
+        
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);    // push the node if ab uske saare baap are in queue
+                }
+            }
+            
+        }
+        if(ans.size()==V){
+            return ans;      // acc to demand
+        }
+        else{
+            return {};
+        }
+    }
+};
