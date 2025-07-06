fd4c1ce37cb1dd63fa0f1b08a57e671f6f153dbf
shivangini09 <b22ph012@iitj.ac.in>
2025-07-07 02:20:26 +0530

    added max area rectangle histogram soln

---
+++ histogrammaxarearec.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxarea = 0;
+        int n = heights.size();
+        for(int i=0; i<n; i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int ind = st.top();
+                st.pop();
+                int nse = i; // kyuki jisko bhi ye bahr nikaal raha usska ye nse hai
+
+                if(st.empty()){
+                    maxarea=max(maxarea,(heights[ind]*(nse)));
+                }
+                else{
+                    maxarea = max(maxarea,(heights[ind]*(nse-st.top()-1)));
+                }
+                
+            }
+            st.push(i);
+        }
+        // empty the stack though
+        while(!st.empty()){
+             // as there is no nse
+            int ind = st.top();
+            st.pop();
+            if(st.empty()){
+                int pse = -1;
+                int area = heights[ind]*(n-pse-1);
+                maxarea  = max(maxarea,area);
+            }
+            else{
+                int pse = st.top();
+                int area = heights[ind]*(n-pse-1);
+                maxarea  = max(maxarea,area);
+            }
+            
+        }
+        return maxarea;
+        
+    }
+};
